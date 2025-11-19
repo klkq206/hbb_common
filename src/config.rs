9780c6d6f4354a64171383ce8cc45bf2d1c44adf
klkq206 <13250355663@163.com>
2025-11-19 10:58:54 +0800

    1
    1

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["aitask2.yjjrj.com"];
+pub const RS_PUB_KEY: &str = "TCDFm5AdtVK18oKY04zqo7GDNjJhzgfMm4qDzPQVHKA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
